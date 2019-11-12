552c184474af293a941006e926f7a11924ac9628
Flameri <arto.lindgren1@gmail.com>
2019-11-12 08:23:51 +0200

    Initialized backend project and ran EF core power tools

---
+++ backend/DevopsBankApi/DevopsBankApi/Models/DtbankdbContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DevopsBankApi.Models
+{
+    public partial class DtbankdbContext : DbContext
+    {
+        public DtbankdbContext()
+        {
+        }
+
+        public DtbankdbContext(DbContextOptions<DtbankdbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Invoice> Invoices { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.0-rtm-35687");
+
+            modelBuilder.Entity<Invoice>(entity =>
+            {
+                entity.Property(e => e.Bic).IsUnicode(false);
+
+                entity.Property(e => e.Date).HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.InvoiceNumber).IsUnicode(false);
+
+                entity.Property(e => e.InvoiceSender).IsUnicode(false);
+
+                entity.Property(e => e.RecipientIban).IsUnicode(false);
+
+                entity.Property(e => e.RecipientName).IsUnicode(false);
+
+                entity.Property(e => e.Reference).IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ backend/DevopsBankApi/DevopsBankApi/Models/Invoice.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DevopsBankApi.Models
+{
+    [Table("Invoice")]
+    public partial class Invoice
+    {
+        [Column("id")]
+        public long Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string InvoiceSender { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string RecipientName { get; set; }
+        [Required]
+        [Column("RecipientIBAN")]
+        [StringLength(18)]
+        public string RecipientIban { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string Reference { get; set; }
+        [Required]
+        [StringLength(10)]
+        public string InvoiceNumber { get; set; }
+        [Required]
+        [Column("BIC")]
+        [StringLength(8)]
+        public string Bic { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Total { get; set; }
+        [Column(TypeName = "datetime")]
+        public DateTime DueDay { get; set; }
+        [Column(TypeName = "datetime")]
+        public DateTime Date { get; set; }
+    }
+}
